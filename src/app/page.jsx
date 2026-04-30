b7ad412c9f73832be3d2bc5e5314d2f09de19651
mdwahidullah0243 <mdwahidullah0243@gmail.com>
2026-04-30 15:15:38 +0600

    Added a scrolling line in the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
 import Banner from '@/components/home/Banner';
-import { Button } from '@heroui/react';
+import MarqueeContent from '@/components/home/MarqueeContent';
+import Marquee from 'react-fast-marquee';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+
+            <div className='bg-red-100 space-x-8 py-3 px-5 flex items-center '>
+                <button className='btn text-white bg-[#D72050] text-xl font-medium'>Latest</button>
+                
+                <Marquee>
+                    <MarqueeContent />
+                </Marquee>
+            </div>
         </div>
     );
 };
---
+++ src/components/home/MarqueeContent.jsx
@@ -0,0 +1,20 @@
+
+const MarqueeContent = () => {
+    return (
+        <div className="flex gap-8 font-medium text-[#2c64ffdd]">
+            <p>
+                <span className="text-lg font-semibold text-[#8403fdf8]">New Arrivals:</span> [Tile Name]
+            </p>
+            <span>|</span>
+            <p>
+                <span className="text-lg font-semibold text-[#d403fdf8]">Weekly Feature:</span> Modern Geometric Patterns
+            </p>
+            <span>|</span>
+            <p>
+                Join the Community...
+            </p>
+        </div>
+    );
+};
+
+export default MarqueeContent;
